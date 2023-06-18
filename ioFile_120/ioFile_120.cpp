cc912f75508babe463c68167342b1fad04e0751a
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-06-19 11:36:36 +1200

    membuka dan membaca file

--- ioFile_120/ioFile_120.cpp
+++ ioFile_120/ioFile_120.cpp
@@ -31,4 +31,7 @@ int main() {
 	//menunjuk ke sebuah file
 	infile.open("contohfile.txt");
 
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
 	
