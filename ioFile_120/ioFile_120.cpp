6f7f01ff54f77ce01dcb6c35896815fe1b4d9113
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-06-19 11:35:28 +1200

    menutup file, membuka dan menunjukkan nama file

--- ioFile_120/ioFile_120.cpp
+++ ioFile_120/ioFile_120.cpp
@@ -22,4 +22,13 @@ int main() {
 		if (baris == "q") break;
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt");
+
 	
