fa0cab4e40f8d8e0fa63eef6dbd51b6c86d46df1
aryanithahajrahmawaddah <aryanithahajrah@gmail.com>
2023-06-19 11:40:32 +1200

    throw kondisi ketika melakukan kesalahan dalam blok try

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -8,14 +8,4 @@ int main()
 		//throw 0.5 //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
-	catch (int a) {
-		//blok ini akan dieksekusi
-		cout << "Pengecualian akan dieksekusi" << endl;
-	}
-	catch (...) {
-		/*jika selain integer maka block ini akan dieksekusi*/
-		cout << "default pengecualian diesekusi" << endl;
-	}
-
-	return 0;
-}
+	
